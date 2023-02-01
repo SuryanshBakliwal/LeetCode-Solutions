22326a6d6b5ebf2442ea9e9132527a05aba06964
Suryansh Bakliwal <90885730+SuryanshBakliwal@users.noreply.github.com>
2023-02-01 20:43:29 +0530

    Time: 1 ms (85.83%), Space: 41.9 MB (78.94%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -4,18 +4,20 @@ class Solution {
             return "";
         }
         int len = str1.length();
-    int len2 = str2.length();
-    int gcd = GCD(len, len2);
-    if (len < len2) {
-        return str1.substring(0, gcd);
-    }
-    return str2.substring(0, gcd);
-}
+        int len2 = str2.length();
+        int gcd = GCD(len, len2);
+        if (len < len2) {
+            return str1.substring(0, gcd);
+        }
+        return str2.substring(0, gcd);
+    }   
 
-public int GCD(int a, int b) {
-    if (b == 0) {
-        return a;
+    public int GCD(int a, int b) {
+        while(a%b != 0){
+            int rem = a%b ;
+            a = b;
+            b = rem;
+        }
+        return b;
     }
-    return a % b == 0 ? b : GCD(b, a % b);
-}
 }
