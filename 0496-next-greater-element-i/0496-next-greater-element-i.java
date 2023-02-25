5fd609069f9fbc65327a785db2d9b4ceea9fbc29
Suryansh Bakliwal <90885730+SuryanshBakliwal@users.noreply.github.com>
2023-02-25 19:10:10 +0530

    Time: 4 ms (43.41%), Space: 42.6 MB (37.80%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,23 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         // first calculate nge for every elem of nums2;
-        int nge[] = new int[nums2.length];
+        Map<Integer, Integer> map = new HashMap<>();
         Stack<Integer> st = new Stack<>();
         for(int i=nums2.length-1; i>=0; i--){
             while(st.size() > 0 && st.peek() <= nums2[i]) 
                 st.pop();
             if(st.size() == 0)
-                nge[i] = -1;
-            else nge[i] = st.peek();
+                map.put(nums2[i], -1);
+            
+            else map.put(nums2[i], st.peek());
             st.push(nums2[i]);
         }
         int res[] = new int[nums1.length];
         for(int i = 0; i<nums1.length; i++){
-            for(int j=0; j<nums2.length; j++){
-                if(nums1[i] == nums2[j]){
-                    res[i] = nge[j];
-                }
-            }
+            int x = nums1[i];
+            res[i] = map.get(x);
         }
         
         return res;
