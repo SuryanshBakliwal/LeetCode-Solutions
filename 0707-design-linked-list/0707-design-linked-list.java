ca1f50cf4ad7dc9b75f34ae72b554d4e8a9471c7
Suryansh Bakliwal <90885730+SuryanshBakliwal@users.noreply.github.com>
2023-03-03 19:34:47 +0530

    Time: 7 ms (87.76%), Space: 42.9 MB (48.47%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.java
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -11,15 +11,12 @@ class MyLinkedList {
     private Node head;
     private Node tail;
     private int size;
-    
-    /** Initialize your data structure here. */
     public MyLinkedList() {
         this.head = null;
         this.tail = null;
         this.size = 0;
     }
     
-    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
     public int get(int index) {
         if(index < 0 || index >= this.size) return -1;
         
@@ -39,7 +36,6 @@ class MyLinkedList {
         return curr;
     }
     
-    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
     public void addAtHead(int val) {
         Node node = new Node(val);
         if(this.size == 0){
@@ -53,22 +49,20 @@ class MyLinkedList {
         this.size++;
     }
     
-    /** Append a node of value val to the last element of the linked list. */
     public void addAtTail(int val) {
         Node node = new Node(val);
         if(this.size == 0){
-            this.head = node;
-            this.tail = node;
+            addAtHead(val);
         }
         else{
             this.tail.next = node;
             node.next = null;
             this.tail = node;
+            this.size++;
         }
-        this.size++;
+        
     }
     
-    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
     public void addAtIndex(int index, int val) {
         if(index < 0 || index > this.size) return;
         
