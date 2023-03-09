a654bea21c89842eb301f7b572625cdfe42bbb1a
Suryansh Bakliwal <90885730+SuryanshBakliwal@users.noreply.github.com>
2023-03-09 20:06:29 +0530

    Time: 11 ms (78.08%), Space: 50.7 MB (52.38%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,22 +10,29 @@
  */
 class Solution {
     
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null) return l2;
-        if(l2 == null) return l1;
-        
-        ListNode handler;
-        if(l1.val < l2.val) {
-            handler = l1;
-            handler.next = mergeTwoLists(l1.next, l2);
-        } else {
-            handler = l2;
-            handler.next = mergeTwoLists(l1, l2.next);
+    public ListNode merge(ListNode l1, ListNode l2) {
+        ListNode l = new ListNode(0), p = l;
+
+        while (l1 != null && l2 != null) {
+          if (l1.val < l2.val) {
+            p.next = l1;
+            l1 = l1.next;
+          } else {
+            p.next = l2;
+            l2 = l2.next;
+          }
+          p = p.next;
         }
-        
-        return handler;       
-    }
-    
+
+        if (l1 != null)
+          p.next = l1;
+
+        if (l2 != null)
+          p.next = l2;
+
+        return l.next;
+      }
+
     public ListNode sortList(ListNode head) {
         if (head == null || head.next == null)
             return head;
@@ -46,6 +53,6 @@ class Solution {
         ListNode l2 = sortList(slow);
 
         // step 3. merge l1 and l2
-        return mergeTwoLists(l1, l2);
+        return merge(l1, l2);
     }
 }
