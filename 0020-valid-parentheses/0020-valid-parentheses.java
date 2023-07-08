5ace63a6812b5416579266756f9dd9f90d5b7546
Suryansh Bakliwal <90885730+SuryanshBakliwal@users.noreply.github.com>
2023-07-08 19:29:05 +0530

    Time: 2 ms (91.72%), Space: 40.5 MB (73.73%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,32 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stk = new Stack<>();
-        for(int i=0; i<s.length(); i++){
+        if(s.length() == 1 || s.length() == 0) return false;
+        Stack<Character> st = new Stack<>();
+        st.push(s.charAt(0));
+        int i=1;
+        while(i < s.length()){
             char ch = s.charAt(i);
-            if(ch == '(' || ch == '{' || ch == '['){
-                stk.push(ch);
-            }else if(ch == ')'){
-                if(stk.size() == 0 || stk.peek() != '('){
+            if(ch == ')'){
+                if( st.size() == 0 || st.peek() != '('){
                     return false;
-                }else stk.pop();
+                }
+                else st.pop();
             }else if(ch == '}'){
-                if(stk.size() == 0 || stk.peek() != '{'){
+                if( st.size() == 0 || st.peek() != '{'){
                     return false;
-                }else stk.pop();
+                }
+                else st.pop();
             }else if(ch == ']'){
-                if(stk.size() == 0 || stk.peek() != '['){
+                if( st.size() == 0 || st.peek() != '[' ){
                     return false;
-                }else stk.pop();
+                }
+                else st.pop();
+            }else{
+                st.push(ch);
             }
+            i++;
         }
-        if(stk.size() == 0)  return true;
-        else return false;
+         if(st.size() == 0)  return true;
+        return false;
     }
 }
