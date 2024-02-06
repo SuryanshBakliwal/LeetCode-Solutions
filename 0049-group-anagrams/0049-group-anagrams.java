44f9ec277c7b5546b463e8c1b82fcd2a12c4d622
Suryansh Bakliwal <90885730+SuryanshBakliwal@users.noreply.github.com>
2024-02-06 13:57:41 +0530

    Time: 10 ms (32.09%), Space: 47.5 MB (65.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,44 +1,15 @@
 class Solution {
-    
-    public boolean checkAnagrams(String s, String t) {
-        if(s.length() != t.length()) return false;
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0)+1);
-        }
-        for(int i=0; i<t.length(); i++){
-            char ch = t.charAt(i);
-            if(map.containsKey(ch)){
-                if(map.get(ch) != 0){
-                    map.put(ch, map.get(ch)-1);
-                }
-                else return false; 
-            }else{
-                return false;
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
+            char c[] = s.toCharArray();
+            Arrays.sort(c);
+            String sortedS = new String(c);
+            if(!map.containsKey(sortedS)){
+                map.put(sortedS, new ArrayList<>());
             }
+            map.get(sortedS).add(s);
         }
-        return true;
-    }
-    
-    
-    public List<List<String>> groupAnagrams(String[] s) {
-        List<List<String>> ans = new ArrayList<>();
-        
-        for(int i=0; i<s.length; i++){
-            if(s[i] != "-1") {
-                List<String> temp = new ArrayList<>();
-                temp.add(s[i]);
-                
-                for(int j=i+1; j<s.length; j++){
-                    if(checkAnagrams(s[i], s[j])){
-                        temp.add(s[j]);
-                        s[j] = "-1";
-                    }
-                }
-                ans.add(temp);
-            }
-        }
-        return ans;
+        return new ArrayList<>(map.values());
     }
 }
