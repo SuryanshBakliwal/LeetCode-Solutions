1a36f23ea06f51b2cbfc799d1e2f85c9cb53c736
Suryansh Bakliwal <90885730+SuryanshBakliwal@users.noreply.github.com>
2024-02-10 13:32:15 +0530

    Time: 13 ms (28.53%), Space: 44.6 MB (34.00%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,32 +1,29 @@
 class Solution {
-    public boolean palindrome(String subs){
-          int li = 0;
-          int ri = subs.length()-1;
-          while(li<=ri){
-              char f = subs.charAt(li);
-              char l = subs.charAt(ri);
-              if(f != l){
-                  return false;
-              }
-              
-              li++;
-              ri--;
-          }
-          return true;
-      }
-    public int countSubstrings(String str) {
-    //write your code here
-        int count = 0;
-        for(int i=0;i<str.length(); i++){
-            for(int j = i+1; j<=str.length(); j++){
-                
-                if(palindrome(str.substring(i,j))){
-                    count++;
+    public int countSubstrings(String s) {
+        int n = s.length();
+        boolean dp[][] = new boolean[n][n];
+        int ans=0;
+        for(int di=0; di<n; di++){
+            for(int i=0, j=di;  j<dp.length; i++, j++){
+            
+                if(di==0){
+                    dp[i][j] = true;
+                    ans++;
+                }else if(di==1){
+                    if(s.charAt(i) == s.charAt(j)){
+                        dp[i][j] = true;
+                        ans++;
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }else{
+                    if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1] == true){
+                        dp[i][j] = true; 
+                        ans++;
+                    }else dp[i][j] = false;
                 }
-                
             }
         }
-        
-        return count;
-      }
+        return ans;
+    }
 }
