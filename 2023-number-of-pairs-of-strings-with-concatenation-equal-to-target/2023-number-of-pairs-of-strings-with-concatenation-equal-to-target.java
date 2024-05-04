4387f3054bcbb6606109dd9b9afb78e62fdc74e5
Suryansh Bakliwal <90885730+SuryanshBakliwal@users.noreply.github.com>
2024-05-04 14:45:40 +0530

    Time: 4 ms (79.14%), Space: 42.7 MB (85.38%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
@@ -1,14 +1,21 @@
 class Solution {
     public int numOfPairs(String[] nums, String target) {
         int ans=0;
+        Map<String, Integer> map = new HashMap<>();
         for(int i=0; i<nums.length; i++){
-            for(int j=i+1; j<nums.length; j++){
-                String curr = nums[i] + nums[j] + "";
-                String rev = nums[j] + nums[i] + "";
-                System.out.println(curr + " " + rev);
-                if(curr.equals(target) == true) ans++;
-                if(rev.equals(target) == true) ans++;
+            if(target.startsWith(nums[i]) == true){
+                String rem = target.substring(nums[i].length()); 
+                if(map.containsKey(rem)){
+                    ans += map.get(rem);
+                }
             }
+            if(target.endsWith(nums[i])){
+                String rem =target.substring(0, target.length() - nums[i].length()); 
+                if(map.containsKey(rem)){
+                    ans += map.get(rem);
+                }
+            }
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
         return ans;
     }
