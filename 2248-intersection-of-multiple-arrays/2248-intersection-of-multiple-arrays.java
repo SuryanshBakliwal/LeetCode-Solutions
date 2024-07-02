5ca406226ef6ac67750ac45b86b6bf76710fa4d3
Suryansh Bakliwal <90885730+SuryanshBakliwal@users.noreply.github.com>
2024-07-02 14:19:19 +0530

    Time: 3 ms (71.00%), Space: 44.6 MB (66.60%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
@@ -1,17 +1,17 @@
 class Solution {
     public List<Integer> intersection(int[][] nums) {
-        Map<Integer, Integer> freq = new HashMap<>();
+        int count[] = new int[1001];
         List<Integer> ans = new ArrayList<>();
         for(int i=0; i<nums.length; i++){
             for(int num : nums[i]){
-                freq.put(num, freq.getOrDefault(num, 0)+1);
+                count[num]++;
             }
         }
         
-        for(int i : freq.keySet()){
-            if(freq.get(i) == nums.length) ans.add(i);
+        for(int i=0; i<count.length; i++){
+            if(count[i] == nums.length) ans.add(i);
         }
-        Collections.sort(ans);
+        
         return ans;
     }
 }
