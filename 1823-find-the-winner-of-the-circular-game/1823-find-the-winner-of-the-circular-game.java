ccf98398519b1f67479d418badc718cc8399a981
Suryansh Bakliwal <90885730+SuryanshBakliwal@users.noreply.github.com>
2024-07-08 15:21:54 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (58.65%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,14 +1,10 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        LinkedList<Integer> player = new LinkedList<>();
-        for(int i=1; i<=n; i++) player.add(i);
-        int lastPlayerRemoved = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<k-1; j++){
-                player.add(player.poll());
-            }
-            lastPlayerRemoved = player.poll();
-        }
-        return lastPlayerRemoved;
+        return solve(n, k) + 1;
+    }
+    
+    public int solve(int n, int k){
+        if(n == 1) return 0;
+        return (solve(n-1, k)+k) % n;
     }
 }
