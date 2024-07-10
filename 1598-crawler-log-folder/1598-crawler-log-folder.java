23707b53f0f5f4b239eb8bbc74adc457484b7df2
Suryansh Bakliwal <90885730+SuryanshBakliwal@users.noreply.github.com>
2024-07-10 16:20:43 +0530

    Time: 1 ms (99.42%), Space: 41.7 MB (81.60%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -1,17 +1,13 @@
 class Solution {
     public int minOperations(String[] logs) {
-        int currPos = 0;
-        for(int i=0; i<logs.length; i++){
-            String s[] = logs[i].split("/");
-            if(s[0].equals("..")){
-                if(currPos != 0) currPos--; 
-            }else if(s[0].equals(".")){
-                continue;
-            }else{
-                currPos++;
+        int step = 0;
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (step > 0) step--;
+            } else if (!log.equals("./")) {
+                step++;
             }
-            System.out.println(s[0] + " " + currPos);
         }
-        return currPos;
+        return step;
     }
 }
