821c00d1c93b1af919c10211a3fb54df844008e0
Suryansh Bakliwal <90885730+SuryanshBakliwal@users.noreply.github.com>
2024-11-03 11:51:26 +0530

    Time: 30 ms (77.77%), Space: 45.5 MB (78.01%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.java
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.java
@@ -1,18 +1,16 @@
 class Solution {
     public String makeFancyString(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        int cnt=0, j=0, i=0;
-        while(i<sb.length()){
-           if(sb.charAt(i) == sb.charAt(j)){
-               cnt++; i++;
-           }else{
-               cnt=0;
-               j=i;
-           }
-            if(cnt == 3){
-                sb.deleteCharAt(j+1); cnt--; i--;
+        StringBuilder sb = new StringBuilder();
+        int cnt=1;
+        char prev=s.charAt(0);
+        sb.append(s.charAt(0));
+        for(int i=1; i<s.length(); i++){
+            if(s.charAt(i) == prev) cnt++;
+            else{
+                prev = s.charAt(i);
+                cnt=1;
             }
-
+            if(cnt < 3) sb.append(s.charAt(i));
         }
         return sb.toString();
     }
